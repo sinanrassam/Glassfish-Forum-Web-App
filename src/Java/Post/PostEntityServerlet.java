94b575bcb2660fe272e3afa6105432bfabad8083
Sinan Rassam <sinanrassam@outlook.com>
2020-03-16 09:26:55 +1300

    Fixed Post Entity Serverlet

--- src/Java/Post/PostEntityServerlet.java
+++ src/Java/Post/PostEntityServerlet.java
@@ -6,17 +6,12 @@
 package Post;
 
 import java.io.IOException;
-import java.util.logging.Logger;
-import javax.annotation.Resource;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.servlet.RequestDispatcher;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.UserTransaction;
 
 /**
  *
@@ -24,16 +19,6 @@ import javax.transaction.UserTransaction;
  */
 @WebServlet(name = "PostEntityServerlet", urlPatterns = {"/createPost"})
 public class PostEntityServerlet extends HttpServlet {
-
-    private Logger logger;
-    @PersistenceContext
-    private EntityManager entityManager;
-    @Resource
-    private UserTransaction userTransaction;
-
-    public PostEntityServerlet() {
-        logger = Logger.getLogger(getClass().getName());
-    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -47,16 +32,57 @@ public class PostEntityServerlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String servletPath = request.getServletPath();
-        logger.info("Test");
-
-        if (servletPath.equals("/createPost")) {
-            logger.info("Create Post");
-
-            request.getSession().setAttribute("message", "Create Post");
-            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login.jsp");
-            dispatcher.forward(request, response);
-
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PostEntityServerlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PostEntityServerlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
