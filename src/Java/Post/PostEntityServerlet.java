cb8340cd240181cbafb1bcf09278f5c72879b6a3
Sinan Rassam <sinanrassam@outlook.com>
2020-03-16 09:29:43 +1300

    Added user logged in check

--- src/Java/Post/PostEntityServerlet.java
+++ src/Java/Post/PostEntityServerlet.java
@@ -5,13 +5,21 @@
  */
 package Post;
 
+import User.User;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
 
 /**
  *
@@ -19,6 +27,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "PostEntityServerlet", urlPatterns = {"/createPost"})
 public class PostEntityServerlet extends HttpServlet {
+
+    private Logger logger;
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Resource
+    private UserTransaction userTransaction;
+
+    public PostEntityServerlet() {
+        logger = Logger.getLogger(getClass().getName());
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,32 +49,33 @@ public class PostEntityServerlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PostEntityServerlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PostEntityServerlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String servletPath = request.getServletPath();
+        HttpSession session = request.getSession(true);
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            request.getSession().setAttribute("error", "You need to be logged in to see this page");
+            RequestDispatcher dispatcher = getServletContext().
+                    getRequestDispatcher("/login.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            if (servletPath.equals("/createPost")) {
+            }
         }
     }
+}
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+/**
+ * Handles the HTTP <code>GET</code> method.
+ *
+ * @param request servlet request
+ * @param response servlet response
+ * @throws ServletException if a servlet-specific error occurs
+ * @throws IOException if an I/O error occurs
+ */
+@Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -70,7 +89,7 @@ public class PostEntityServerlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -81,7 +100,7 @@ public class PostEntityServerlet extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-    public String getServletInfo() {
+        public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
