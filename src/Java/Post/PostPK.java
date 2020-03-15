5f5afed29ecadd7192e1d1874142edd994bc671d
Sinan Rassam <sinanrassam@outlook.com>
2020-03-16 08:51:32 +1300

    Completed PostPK Class

--- src/Java/Post/PostPK.java
+++ src/Java/Post/PostPK.java
@@ -5,10 +5,42 @@
  */
 package Post;
 
+import User.UserPK;
+import java.io.Serializable;
+
 /**
  *
  * @author sinan.rassam
  */
-public class PostPK {
-    
+public class PostPK implements Serializable {
+    // note that fields in public key class must be public
+
+    public Integer id;
+
+    public PostPK() {
+    }
+
+    public PostPK(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof PostPK)) {
+            return false;
+        } else {
+            PostPK other = (PostPK) obj;
+            return id.equals(other.id);
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return 0;
+        } else {
+            return id.hashCode();
+        }
+    }
+
 }
