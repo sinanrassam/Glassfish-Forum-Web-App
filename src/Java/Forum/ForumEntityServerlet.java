05b473cf0c50adfc9140d0ff4ceced7b3a8bfb9c
Sinan Rassam <sinanrassam@outlook.com>
2020-03-17 11:06:39 +1300

    Added serverlet logic for getting forums

--- src/Java/Forum/ForumEntityServerlet.java
+++ src/Java/Forum/ForumEntityServerlet.java
@@ -3,12 +3,133 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Forum;
+package Post;
+
+import Forum.Forum;
+import User.User;
+import User.UserEntityServerlet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 /**
  *
  * @author sinan.rassam
  */
-public class ForumEntityServerlet {
-    
+@WebServlet(name = "ForumEntityServerlet", urlPatterns = {"/getForums"})
+public class ForumEntityServerlet extends HttpServlet {
+
+    private Logger logger;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public ForumEntityServerlet() {
+        logger = Logger.getLogger(getClass().getName());
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String servletPath = request.getServletPath();
+        HttpSession session = request.getSession(true);
+        User user = (User) session.getAttribute("user");
+
+        if (user == null) {
+            request.getSession().setAttribute("error", "You need to be logged in to see this page");
+            RequestDispatcher dispatcher = getServletContext().
+                    getRequestDispatcher("/login.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            if (servletPath.equals("/getForums")) {
+                if (entityManager != null) {
+                    String jpqlCommand = "SELECT f FROM forums f";
+                    Query query = entityManager.createQuery(jpqlCommand);
+
+                    List<Forum> forums = new ArrayList<>();
+
+                    if (query.getResultList().size() > 0) {
+                        logger.info("Forums found:");
+
+                        for (int i = 0; i < query.getResultList().size(); i++) {
+                            forums.add((Forum) query.getResultList().get(i));
+                        }
+                    } else {
+                        logger.info("No Forums found!");
+                    }
+
+                    request.setAttribute("forums", forums);
+                    RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/forum.jsp");
+                    dispatcher.forward(request, response);
+                }
+            }
+        }
+
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
